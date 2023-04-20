fe07d19a3adadb54cb5b2feeb6297c009cbd6ef6
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-04-20 09:16:42 +0200

    Initial Commit

---
+++ src/main/java/memory/MemoryApp.java
@@ -0,0 +1,22 @@
+package memory;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MemoryApp extends Application {
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// La on est vraiment sur la version minimale
+		// Mais les étudiants vont bientôt en faire une application jouable !
+		
+		primaryStage.show();
+	}
+	
+	public static void main2(String[] args) {
+		Application.launch(args);
+	}
+
+}
